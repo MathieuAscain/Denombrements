7dfebb07f6a6dd03c4aa27001cbfcf63e987180f
MathieuAscain <mathieu.seligmann@gmail.com>
2020-11-02 10:54:50 +0100

    Correction of FOR loops

--- Denombrements/Program.cs
+++ Denombrements/Program.cs
@@ -1,42 +1,69 @@
-﻿using System;
+﻿
+/**
+ * title : counting calculation
+ * goal : allows 3 types of calculation : permutation, arrangement, counting
+ * author : Mathieu Seligmann
+ * creation date : October, 29th 2020
+ * Last update : November, 2nd 2020
+ * */
+using System;
 
 namespace Denombrements
 {
     class Program
     {
-
-        public static int ShowElement()
+        ///
+        /// <summary>
+        /// Multiplication calculation of many consecutive integers, from startValue to endValue
+        /// </summary>
+        /// <param name="startValue">starting value for the calculation</param>
+        /// <param name="endValue">ending value for the calculation</param>
+        /// <returns>result of the multiplication</returns>
+        static long IntMultiplication(int startValue, int endValue)
         {
-            Console.Write("Total number of elements to be managed = ");
-            int n = int.Parse(Console.ReadLine());
-            return n;
+            long multiplication = 1;
+            for (int k = startValue; k <= endValue; k++)
+                multiplication *= k;
+            return multiplication;
         }
 
-        public static int ShowSubElement()
-        {
-            Console.Write("Number of elements in the subassy = ");
-            int n = int.Parse(Console.ReadLine());
-            return n;
-        }
+        ///
+        /// <summary>
+        /// User selection control
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns>integer selected by user</returns>
+        static int UserSelection(String message)
+            {
+                int number = 0;
+                Console.Write(message);
+                try
+                {
+                    number = int.Parse(Console.ReadLine());
+                    Console.WriteLine();
+                }
 
-        public static long Permutation(int n)
-        {
-            long r = 1;
-            for (int k = 1; k <= n; k++)
-                r *= k;
-            return r;
-        }
+                catch
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Wrong input, enter an integer");
+                }
 
-        public static long Arrangement(int t, int n)
-        {
-            long r = 1;
-            for (int k = (t - n + 1); k <= t; k++)
-                r *= k;
-            return r;
-        }
+                return number;
+
+            }
+
+        ///
+        /// <summary>
+        /// Menu allowing to make 3 choices : permutation, arrangement, combination
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             char choice = '1';
+            int nbrSubAssy = 0;
+            int totalNumber = 0;
+            long arrangement = 0;
             while (choice != '0')
             {
                 Console.WriteLine("Permutation ...................... 1");
@@ -53,34 +80,18 @@ namespace Denombrements
                         Environment.Exit(0);
                         break;
                     case '1':
-                        // number of elements selection
-                        int n1 = ShowElement();
-                        // permutation calculation
-                        long rPermutation = Permutation(n1);
-                        Console.WriteLine(n1 + "! = " + rPermutation);
+                        totalNumber = UserSelection("Total number of elements to be taken into account");
+                        long permutation = IntMultiplication(1, totalNumber);
+                        Console.WriteLine(totalNumber + "! = " + permutation);
                         break;
                     case '2':
-                        // number of elements selection
-                        int t2 = ShowElement();
-                        // number of elements in the subassy
-                        int n2 = ShowSubElement();
-                        // Arrangement calculation
-                        long rArrangement = Arrangement(t2, n2);
-                        //Console.WriteLine("result = " + (r1 / r2));
-                        Console.WriteLine("A(" + t2 + "/" + n2 + ") = " + rArrangement);
+                        nbrSubAssy = UserSelection("Total number of elements in the subassy to be taken into account");
+                        arrangement = IntMultiplication(totalNumber - nbrSubAssy + 1, totalNumber);
+                        Console.WriteLine("A(" + totalNumber + "/" + nbrSubAssy + ") = " + arrangement);
                         break;
                     case '3':
-                        // number of elements selection
-                        int t3 = ShowSubElement();
-                        // number of elements in the subassy
-                        int n3 = ShowSubElement();
-                        // Arrangement calculation
-                        long r1 = Arrangement(t3, n3);
-                        // permutation calculation 
-                        long r2 = Permutation(n3);
-                        // combination calculation
-                        //Console.WriteLine("result " + (r1 / r2));
-                        Console.WriteLine("C(" + t3 + "/" + n3 + ") = " + (r1 / r2));
+                        long combination = arrangement / IntMultiplication(1, nbrSubAssy);
+                        Console.WriteLine("C(" + totalNumber + "/" + nbrSubAssy + ") = " + combination);
                         break;
                     default:
                         Console.WriteLine("Wrong input");
